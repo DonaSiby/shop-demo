5df4619597a92ac3830eac6ca759d168c4286553
Dona Siby <dona.c.siby@gmail.com>
2023-04-09 22:10:32 +0530

    welcome screen page edited

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,19 +1,65 @@
 import React from 'react';
 import { 
+    Image,
     ImageBackground,
-    StyleSheet
+    StyleSheet,
+    View,
+    Text
  } from 'react-native';
 
-function WelcomeScreen(props) {
+function WelcomeScreen() {
     return (
         <ImageBackground 
-        style={styles.background}
-        source={require('../assets/background.jpg')}></ImageBackground>
+            style={styles.background}
+            source={require('../assets/background.jpg')}>
+                <View style={styles.logoContainer}>
+                    <Image
+                        style={styles.logo}
+                        source={require('../assets/logo-red.png')}
+                    />
+                    <Text>
+                        Sell What You Don't Need
+                    </Text>
+                </View>
+                
+                <View
+                    style={styles.loginButton}>
+                </View>
+                <View
+                    style={styles.registerButton}>
+                </View>
+        </ImageBackground>
     );
 }
 const styles = StyleSheet.create({
     background:{
-        flex:1 //image in the full screen
+        flex:1, //image in the full screen
+        justifyContent:'flex-end',//using justify content we can align items along primary axis
+        alignItems:'center'//align items on secondary axis
+    },
+   
+    loginButton:{
+        width:'100%',
+        height:70,
+        backgroundColor:'#fc5c65'
+
+    },
+
+    logo:{
+        width:100,
+        height:100,
+    },
+
+    logoContainer:{
+        position:'absolute',
+        top:70,
+        alignItems:'center'
+    },
+
+    registerButton:{
+        width:'100%',
+        height:70,
+        backgroundColor:'#4ecdc4'
 
     }
 })
